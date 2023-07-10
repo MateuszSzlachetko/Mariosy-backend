963ef1755d765221a0f40ee1251aab56f223c34d
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-10 20:45:25 +0200

    test: Template file for User class tests

---
+++ src/test/java/com/deloitte/ads/mariosy/UserTest.java
@@ -0,0 +1,36 @@
+package com.deloitte.ads.mariosy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserTest {
+
+
+    @Test
+    void getUsername() {
+    }
+
+    @Test
+    void setUsername() {
+    }
+
+    @Test
+    void getMariosy() {
+    }
+
+    @Test
+    void getGivenMariosy() {
+    }
+
+    @Test
+    void giveMarios() {
+    }
+
+    @Test
+    void testGiveMarios() {
+    }
+}
