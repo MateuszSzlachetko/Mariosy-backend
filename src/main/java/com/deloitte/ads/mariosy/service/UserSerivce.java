02e91184cd61344676200674a651a05a6b0ad8b1
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-11 20:47:15 +0200

    feat: Basic user service without Spring decorators

---
+++ src/main/java/com/deloitte/ads/mariosy/service/UserSerivce.java
@@ -0,0 +1,35 @@
+package com.deloitte.ads.mariosy.service;
+
+import com.deloitte.ads.mariosy.repository.User;
+import com.google.common.collect.Sets;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+
+public class UserSerivce {
+
+    private Set<User> users;
+
+    public UserSerivce() {
+        User u1 = new User(1,"Mateusz");
+        User u2 = new User(2,"Bartek");
+        User u3 = new User(3,"Marek");
+        User u4 = new User(4,"Artur");
+        User u5 = new User(5,"Dawid");
+
+        this.users = Sets.newHashSet(u1,u2,u3,u4,u5);
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public User getUserById(long id) {
+            Optional<User> user = users.stream().filter(u -> u.getId() == id).findFirst();
+            if(user.isPresent())
+                return user.get();
+
+            throw new NoSuchElementException("User with such id does not exist");
+    }
+}
