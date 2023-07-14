e03d70de144f58119e7fbf656e29e56948ab79a8
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-14 16:19:59 +0200

    feat: Added User entity

--- src/main/java/com/deloitte/ads/mariosy/entity/User.java
+++ src/main/java/com/deloitte/ads/mariosy/entity/User.java
@@ -1,34 +1,57 @@
-package com.deloitte.ads.mariosy.repository;
+package com.deloitte.ads.mariosy.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.google.common.collect.Sets;
 
 import javax.persistence.*;
-import java.util.ArrayList;
+import java.util.Set;
 
-@Entity(name = "USER")
+@Entity(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private long id;
+    private Long id;
 
     @Column(name = "username")
     private String username;
 
     @Column(name = "email")
     private String email;
-//    private ArrayList<Marios> receivedMariosy;
-//    private ArrayList<Marios> givenMariosy;
 
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "marios_receivers",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "marios_id"))
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Set<Marios> receivedMarios;
+
+    @OneToMany(mappedBy = "author")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Set<Marios> givenMarios;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+        this.receivedMarios = Sets.newHashSet();
+        this.givenMarios = Sets.newHashSet();
+    }
 
     public User() {
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -46,6 +69,32 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Marios> getReceivedMarios() {
+        return receivedMarios;
+    }
+
+    public void setReceivedMarios(Set<Marios> receivedMarios) {
+        this.receivedMarios = receivedMarios;
+    }
+
+    public Set<Marios> getGivenMarios() {
+        return givenMarios;
+    }
+
+    public void setGivenMarios(Set<Marios> givenMarios) {
+        this.givenMarios = givenMarios;
+    }
+
+    public void addMarios(Marios marios) {
+        this.receivedMarios.add(marios);
+        marios.getReceivers().add(this);
+    }
+
+    public void removeMarios(Marios marios) {
+        this.receivedMarios.remove(marios);
+        marios.getReceivers().remove(this);
     }
 }
 
